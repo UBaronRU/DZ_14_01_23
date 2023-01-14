bad493c8e4f1c0dd65da6e2f21ddde3be9b1b3ed
UBaronRU <ubaron@mail.ru>
2023-01-14 19:29:46 +0300

    DZ_08 Удалить вторую цифру трёхзначного числа

--- Program.cs
+++ Program.cs
@@ -14,6 +14,31 @@
 // DZ_07
 //Удалить вторую цифру трёхзначного числа
 // DZ_08
+int Num_100;
+int Num_10;
+int Num_1;
+int Num_not2;
+int Num1;
+Console.Write("Введите число от 100 до 999, а мы удалим вторую цифру : ");
+Num1 = int.Parse(Console.ReadLine());
+if (Num1 >= 100 & Num1 <= 999)
+{
+Num_100 =  Num1 / 100;
+Num_10  = (Num1 - Num_100 * 100) / 10;
+Num_1   = (Num1 - Num_100 * 100 - Num_10 * 10);
+Console.WriteLine("Полный состав ");
+Console.WriteLine("Первая цифра "+ Num_100 +" вторая цифра " + Num_10 +" третья цифра " + Num_1);
+Num_not2 = Num_100*10+Num_1;
+Console.WriteLine("И число без второй цифры " + Num_not2);
+Num_not2 = Num_100*100+Num_1;
+Console.WriteLine("Или так " + Num_not2);
+
+}
+else
+{
+  Console.WriteLine("Ошибка! Число должно быть от 100 до 999");  
+}
+
 //Выяснить, кратно ли число заданному, если нет, вывести остаток.
 // DZ_09
 //Найти третью цифру числа или сообщить, что её нет
